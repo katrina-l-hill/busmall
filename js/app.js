5abf8aac716fe5d81bf55eeef218802c23b0c062
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-21 15:31:32 -0800

    added DOM references for images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
+'use strict'
+
+// DOM references
+
+let myContainer = document.getElementById('container');
+let imgOne = document.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+let imgThree = document.getElementById('img-three');
+let imgFour = document.getElementById('img-four');
+let imgFive = document.getElementById('img-five');
+let imgSix = document.getElementById('img-six');
+let imgSeven = document.getElementById('img-seven');
+let imgEight = document.getElementById('img-eight');
+let imgNine = document.getElementById('img-nine');
+let imgTen = document.getElementById('img-ten');
+let imgEleven = document.getElementById('img-eleven');
+let imgTwelve = document.getElementById('img-twelve');
+let imgThirteen = document.getElementById('img-thirteen');
+let imgFourteen = document.getElementById('img-fourteen');
+let imgFifteen = document.getElementById('img-fifteen');
+let imgSixteen = document.getElementById('img-sixteen');
+let imgSeventeen = document.getElementById('img-seventeen');
+let imgEighteen = document.getElementById('img-eighteen');
+let imgNineteen = document.getElementById('img-nineteen');
+let imgTwenty = document.getElementById('img-twenty');
+
+
+
+
+// Create a constructor function that creates an object associated with each product, and has the following properties:
+
+//     Name of the product
+//     File path of image
+//     Times the image has been shown
+
+
+// Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
+
+
+// For each of the three images, increment its property of times it has been shown by one.
+
+
+// Attach an event listener to the section of the HTML page where the images are going to be displayed.
+
+
+// Once the users ‘clicks’ a product, generate three new products for the user to pick from.
+
 
