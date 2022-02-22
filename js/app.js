7fc49392efb602a4324d83f6e9075e8c6a57869a
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-21 16:38:09 -0800

    added instantiate and render images functions

--- js/app.js
+++ js/app.js
@@ -27,7 +27,12 @@ let imgTwenty = document.getElementById('img-twenty');
 let resultsBtn = document.getElementById('show-results-button');
 let showResults = document.getElementById('display-results-list');
 
-// Constructor
+// Create a constructor function that creates an object associated with each product, and has the following properties:
+
+//     Name of the product
+//     File path of image
+//     Times the image has been shown
+
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
@@ -58,20 +63,45 @@ new Product('water-can');
 new Product('wine-glass');
 new Product('wireframe', 'png');
 
+console.log(allProducts);
 
 
 
-// Create a constructor function that creates an object associated with each product, and has the following properties:
-
-//     Name of the product
-//     File path of image
-//     Times the image has been shown
 
 
 // Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
 
+function getRandomIndex() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+// render images
+function renderImgs() {
+  let productOneIndex = getRandomIndex();
+  let productTwoIndex = getRandomIndex();
+  let productThreeIndex = getRandomIndex();
+  let productFourIndex = getRandomIndex();
+  let productFiveIndex = getRandomIndex();
+  let productSixIndex = getRandomIndex();
+  let productSevenIndex = getRandomIndex();
+  let productEightIndex = getRandomIndex();
+  let productNineIndex = getRandomIndex();
+  let productTenIndex = getRandomIndex();
+  let productElevenIndex = getRandomIndex();
+  let productTwelveIndex = getRandomIndex();
+  let productThirteenIndex = getRandomIndex();
+  let productFourteenIndex = getRandomIndex();
+  let productFifteenIndex = getRandomIndex();
+  let productSixteenIndex = getRandomIndex();
+  let productSeventeenIndex = getRandomIndex();
+  let productEighteenIndex = getRandomIndex();
+  let productNineteenIndex = getRandomIndex();
+  let productTwentyIndex = getRandomIndex();
+}
 
 // For each of the three images, increment its property of times it has been shown by one.
+
+
 
 
 // Attach an event listener to the section of the HTML page where the images are going to be displayed.
